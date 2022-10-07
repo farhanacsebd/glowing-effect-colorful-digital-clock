168ef0dc2b1150b9d96537dc397b28c4b4b0c0fa
farhanacsebd <farhana.cse.bd@gmail.com>
2022-10-08 01:44:59 +0600

    final function done

--- main/app.js
+++ main/app.js
@@ -5,9 +5,25 @@ function digitalClock(){
     let seconds = date.getSeconds();
     let timeFormet = "AM"; 
 
+
+    if(hours === 0){
+        hours = 12;
+    };
+
+    if(hours > 12){
+        hours = hours - 12;
+        timeFormet = "PM";
+    };
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+
     let finalTime = `${hours} :${minutes} :${seconds} ${timeFormet} `;
     document.getElementById('time').innerText = finalTime;
-    // document.getElementById('span').innerText = timeFormet;
+
+    setInterval(digitalClock,1000)
 }
 
-setInterval(digitalClock,1000)
+digitalClock()
